65509cad0d29bda99a649d7d74a1ba80da5f6ff9
Sulayman <sirikone@hotmail.com>
2022-12-11 21:47:50 +0100

    last update

--- controllers/delete-controller.js
+++ controllers/delete-controller.js
@@ -2,11 +2,14 @@
 
 const pool = require("../config");
 
+
 exports.delete_task = async (req, res) => {
   try {
+
     const { id } = req.params;
- console.log("test du path delete",id);
+    console.log("test du path delete", id);
     const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id= $1", [id]);
+
     res.json("todo has been deleted");
   } catch (err) {
     console.error(err.message);
